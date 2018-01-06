f76907c362a567ca3151b7e8dc0bfb3978d28079
Khaliq Gant <khaliqgant@gmail.com>
2018-01-06 18:51:52 +0100

    add in fix commands and precommit commands

--- lastfmApi.ts
+++ lastfmApi.ts
@@ -1,6 +1,13 @@
 export default function user(userId: string): string {
 
-  const foo = 'foo';
-  return foo;
+  const am = 'as';
+  const params = {
+    all: 'as',
+    foo: 'bay',
+  };
+
+  const fool = { bar: 'This is a bar.', baz: { qux: 'This is a qux' }, difficult: 'to read',};
+
+  return am;
 
 }
